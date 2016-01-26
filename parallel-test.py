1730609781dc4a0e1adce42bb80635f7ef6c5beb
Chorna Olga <titovaolga90@gmail.com>
2016-01-26 22:17:57 +0200

    Update parallel-test.py

--- parallel-test.py
+++ parallel-test.py
@@ -24,17 +24,16 @@ if __name__ == '__main__':
     pset = CyclePermutationSet(tuple(el))
     print "PSet", pset.generation_elements
     bb = BranchAndBound(el, co)
-    bb.printbranch(el)
     start3 = time.time()
     bbdaughter = bb.findcyclemin(el)
     finish3 = time.time()
     print "Point and min fuc value found using b&b without restriction: ", (bbdaughter.Adress[1:], bbdaughter.FuncValue)
     print "Time using b&b without restriction: ", (finish3 - start3)
-
-    if is_solution(s, bbdaughter.Adress[1:]):
-        print "Restricrions are not working"
-    else:
-        print "Restricrions are working"
+    print "Is soluthion ", is_solution(s, bbdaughter.Adress[1:])
+    #if is_solution(s, bbdaughter.Adress[1:]):
+        #print "Restricrions are not working"
+    #else:
+        #print "Restricrions are working"
     #start2 = time.time()
     #point2, func_value2 = find_minimum(func, s, pset, quiet=True)
     #finish2 = time.time()
