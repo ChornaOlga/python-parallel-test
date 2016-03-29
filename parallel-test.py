08d47886597111375fd40e0ef847643d3cf20a70
Chorna Olga <titovaolga90@gmail.com>
2016-03-29 18:55:59 +0300

    Update parallel-test.py

--- parallel-test.py
+++ parallel-test.py
@@ -2,40 +2,26 @@ from cycle_permutations import CyclePermutationSet
 from branchandbound import BranchAndBound
 from parallel_random import find_minimum_with_exhaustive_search, find_minimum, is_solution
 import time
+from task import *
 import numpy as np
 
 if __name__ == '__main__':
 
-    print "Get started"
-    n=20
-    s1 = list(np.random.randint(5, size=n)-np.random.randint(8))
-    s2 = list(np.random.randint(5, size=n)-np.random.randint(8))
-    s1.append(0)
-    s2.append(0)
-    s = [s1, s2]
-    print "S=", s
-    co = list(np.random.randint(100, size=n))
-    print "Coef=", co
-    el=[]
-    el.extend(range(1, n+1))
-    print "Elem=", el
-    func = tuple(co)
-    print "Func=", func
-    pset = CyclePermutationSet(tuple(el))
-    print "PSet", pset.generation_elements
-    bb = BranchAndBound(el, co)
+    T = Task(20)
+    CyclePermutationSet = CyclePermutationSet(tuple(T.generation_elements))
+    bb = BranchAndBound(T.generation_elements, T.coefficients)
     start3 = time.time()
-    bbdaughter = bb.findcyclemin(el)
+    bbdaughter = bb.findcyclemin(T.generation_elements)
     finish3 = time.time()
     print "Point and min fuc value found using b&b without restriction: ", (bbdaughter.Adress[1:], bbdaughter.FuncValue)
     print "Time using b&b without restriction: ", (finish3 - start3)
-    if is_solution(s, bbdaughter.Adress[1:]):
+    if is_solution(T.restrictions, bbdaughter.Adress[1:]):
         print "Restricrions are not working"
     else:
         print "Restricrions are working"
     start2 = time.time()
     print "Time started"
-    point2, func_value2 = find_minimum(func, s, pset, quiet=True)
+    point2, func_value2 = find_minimum(T.coefficients, T.restrictions, CyclePermutationSet, quiet=True)
     finish2 = time.time()
     print "Point and min fuc value found using random search: ", (point2, func_value2)
     print "Time using random search: ", (finish2 - start2)
